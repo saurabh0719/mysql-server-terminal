102946b9aff78891e091efb2f1756227eda85e92
Saurabh Pujari <saurabhpuj99@gmail.com>
2019-08-10 19:50:22 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+//Step1
+ $db = mysqli_connect('localhost','root','ggmufc0719','bedrock')
+ or die('Error connecting to MySQL server.');
+?>
+
+<html>
+ <head>
+ </head>
+ <body>
+ <h1>PHP connect to MySQL</h1>
+
+<?php
+//Step2
+$query = "SELECT * FROM student";
+mysqli_query($db, $query) or die('Error querying database.');
+
+$result = mysqli_query($db, $query);
+$row = mysqli_fetch_array($result);
+
+while ($row = mysqli_fetch_array($result)) {
+ echo $row['ID'] . ' ' . $row['Name'] . ': ' . $row['DOB'] . ' ' . $row['Department'] . $row['HomeTown'] . '<br />';
+}
+?>
+
+</body>
+</html>
---
+++ new1.java
@@ -0,0 +1,29 @@
+import java.sql.*;
+import java.io.*;
+public class new1 {
+
+public static void main(String[] args) {
+    // TODO Auto-generated method stub
+    try
+    {  
+        Class.forName("com.mysql.jdbc.Driver");  
+        Connection con=DriverManager.getConnection(  
+        "jdbc:mysql://localhost:3306/bedrock","root","ggmufc0719");  
+
+        Statement stmt=con.createStatement();  
+        ResultSet rs=stmt.executeQuery("select * from student");  
+        while(rs.next())  
+        System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3)+"  "+rs.getString(4)+"  "+rs.getString(5));  
+        con.close();
+    }
+
+    catch(Exception e)
+    {
+        System.out.println(e);
+    }
+
+        }  
+
+}
+
+
